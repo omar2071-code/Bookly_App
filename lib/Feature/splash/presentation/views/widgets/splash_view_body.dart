c4cfbb90bfeca974f3b8b0955affc608191997ad
omar2071-code <omarabdallah2071@gmail.com>
2025-11-12 13:40:04 +0200

    create splah view animation text

--- lib/Feature/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/Feature/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,8 +1,41 @@
 import 'package:bookly/core/utils/asset.dart';
 import 'package:flutter/material.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<Offset> slideAnimation;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    //create animation and give time
+    animationController = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 1),
+    );
+    //select value & select move text from down to up
+    slideAnimation = Tween<Offset>(
+      begin: Offset(0, 5),
+      end: Offset.zero,
+    ).animate(animationController);
+    //start animation
+    animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    animationController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,7 +45,10 @@ class SplashViewBody extends StatelessWidget {
       children: [
         Image.asset(AssetData.kLogo),
         SizedBox(height: 4),
-        Text('Read Free Book', textAlign: TextAlign.center),
+        SlideTransition(
+          position: slideAnimation,
+          child: Text('Read Free Book', textAlign: TextAlign.center),
+        ),
       ],
     );
   }
