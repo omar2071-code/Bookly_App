e2a9af916e71b0ac4befd0c25c31a935b670d7e2
omar2071-code <omarabdallah2071@gmail.com>
2025-11-13 02:26:04 +0200

    refactor init state & navigate home

--- lib/Feature/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/Feature/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,5 +1,8 @@
+import 'package:bookly/Feature/home/presentaion/views/home_view.dart';
+import 'package:bookly/constants.dart';
 import 'package:bookly/core/utils/asset.dart';
 import 'package:flutter/material.dart';
+import 'package:get/route_manager.dart';
 
 class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
@@ -15,24 +18,15 @@ class _SplashViewBodyState extends State<SplashViewBody>
 
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
     //create animation and give time
-    animationController = AnimationController(
-      vsync: this,
-      duration: Duration(seconds: 1),
-    );
-    //select value & select move text from down to up
-    slideAnimation = Tween<Offset>(
-      begin: Offset(0, 5),
-      end: Offset.zero,
-    ).animate(animationController);
-    //start animation
-    animationController.forward();
+    InitSlidingAnimation();
+    NavigateToHome();
   }
 
   @override
   void dispose() {
+    //finish it and close animation
     animationController.dispose();
     super.dispose();
   }
@@ -51,5 +45,30 @@ class _SplashViewBodyState extends State<SplashViewBody>
         ),
       ],
     );
+  }
+
+  void NavigateToHome() {
+    Future.delayed(Duration(seconds: 2), () {
+      Get.to(
+        () => HomeView(),
+        transition: Transition.fade,
+        duration: kTransationDuration,
+      );
+    });
+  }
+
+  void InitSlidingAnimation() {
+    //create animation and give time
+    animationController = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 1),
+    );
+    //select value & select move text from down to up
+    slideAnimation = Tween<Offset>(
+      begin: Offset(0, 5),
+      end: Offset.zero,
+    ).animate(animationController);
+    //start animation
+    animationController.forward();
   }
 }
