441b9ea85a274d335b83165623f7d60933789380
omar2071-code <omarabdallah2071@gmail.com>
2025-11-14 18:59:32 +0200

    news book cubit done

--- lib/Feature/home/presentaion/manager/news_book_cubit/news_book_cubit.dart
+++ lib/Feature/home/presentaion/manager/news_book_cubit/news_book_cubit.dart
@@ -1,9 +1,24 @@
 import 'package:bloc/bloc.dart';
 import 'package:bookly/Feature/home/data/models/book_model/book_model.dart';
+import 'package:bookly/Feature/home/data/repo/home_repo.dart';
 import 'package:equatable/equatable.dart';
 
 part 'news_book_state.dart';
 
 class NewsBookCubit extends Cubit<NewsBookState> {
-  NewsBookCubit() : super(NewsBookInitial());
+  NewsBookCubit(this.homeRepo) : super(NewsBookInitial());
+  final HomeRepo homeRepo;
+
+  Future<void> fetchNewsBook() async {
+    emit(NewsBookLoading());
+    var result = await homeRepo.fetshNewsBook();
+    result.fold(
+      (failure) {
+        emit(NewsBookFailure(failure.errorMessage));
+      },
+      (books) {
+        emit(NewsBookSuccess(books));
+      },
+    );
+  }
 }
